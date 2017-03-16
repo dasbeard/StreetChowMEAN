d4327b68b12c29dcf34952de7a4929592abb3c51
Bryan Jones <thebeard@me.com>
2017-03-15 17:54:46 -0700

    Modularized factories and controllers

--- client/static/js/controllers/logRegController.js
+++ client/static/js/controllers/logRegController.js
@@ -1,24 +1,4 @@
 // =========================================================================
-// =========================== Controller 1 ================================
-// =========================================================================
-app.controller('controller1', function($scope, $location, $cookies){
-  $scope.test = 'Hello';
-
-  if($cookies.getObject('loggedUser')){
-    $scope.organization = $cookies.getObject('loggedUser')
-  }
-
-  $scope.logout = function(){
-    $cookies.remove('loggedUser');
-    $location.url('/logout');
-  } // End Logout
-
-
-
-});
-
-
-// =========================================================================
 // ==================== Login / Register Controller ========================
 // =========================================================================
 app.controller('logReg', function($scope, logRegFactory, $location, $cookies){
@@ -27,9 +7,6 @@ app.controller('logReg', function($scope, logRegFactory, $location, $cookies){
 
 // Register New Organization Method
   $scope.regOrganization = function (){
-    console.log('Register Button Clicked');
-    console.log($scope.reg);
-
     $scope.error = "";
     $scope.user = {};
     if($scope.reg){
@@ -212,12 +189,3 @@ function regValidation(reg){
 
 
 } // End if Scope.Reg
-
-
-
-
-//  =========== Check if user is logged in ==========
-// $scope.user = $cookies.get('loggedUser');
-// if(!$scope.user){
-//   $location.url('/');
-// }
