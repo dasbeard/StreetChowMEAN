a0b817424ca071a424d2c004810f4c5380068b2b
Bryan Jones <thebeard@me.com>
2017-04-23 17:23:04 -0700

    moving to fix log/reg page

--- client/static/js/controllers/showpagecontroller.js
+++ client/static/js/controllers/showpagecontroller.js
@@ -1,61 +1,13 @@
-// =========================================================================
-// =========================== Controller 1 ================================
-// =========================================================================
-app.controller('showPageController', function($scope, $location, showPageFactory, $cookies, $routeParams, $window, NgMap){
-  $scope.org = $routeParams;
-  $scope.thisOrg = $cookies.getObject('loggedUser')
+app.controller('showPageController', function($scope, showPageFactory, $location, $cookies){
+  $scope.user = $cookies.getObject('loggedUser');
 
-  // console.log($scope.org);
-
-  if ($scope.thisOrg){
-    if ($scope.thisOrg.id == $scope.org.id){
-      $scope.showEdit = true;
-    }
-  }
-
-
-  showPageFactory.getInfo ($scope.org, function(output){
-    if(!output){
-      console.log('Someing went wrong');
+  showPageFactory.getShow($scope.user, function(output){
+    if (output.data){
+      $scope.organization = output.data;
     } else {
-      // console.log(output);
-      $scope.thisOrg = output;
-      $scope.thisOrg.phone = phoneDisplay(output.phone)
-      $scope.latLong = output.latitude + ',' + output.longitude
-      map();
+      $scope.error = 'Something went wrong';
     }
-  });
+  })
 
 
-
-  $scope.linkModelFunc = function (){
-    var site = 'http://'
-    site += $scope.thisOrg.website
-    $window.open(site);
-  }
-
-
-var map = function (){
-  NgMap.getMap().then(function(map) {
-      var marker = new google.maps.Marker({
-        position: {lat: $scope.thisOrg.latitude, lng: $scope.thisOrg.longitude},
-        map: map,
-        clickable: false,
-        animation: google.maps.Animation.DROP,
-      })
-  });
-}
-
-
-
-
-}); //End Controller
-
-
-function phoneDisplay(str){
-  if (str.length == 10){
-    return '(' + str.substr(0,3) + ')' + str.substr(3,3) + '-' + str.substr(6);
-  } else {
-    return str.substr(0,1) + '(' + str.substr(1,3) + ')' + str.substr(4,3) + '-' + str.substr(7);
-  }
-};
+}); // End controller
--- client/static/js/factories/showpageFactory.js
+++ client/static/js/factories/showpageFactory.js
@@ -1,16 +1,11 @@
-// =========================================================================
-// ============================ Wall Factory ===============================
-// =========================================================================
 app.factory('showPageFactory', function($http){
   var factory = {};
 
-  factory.getInfo = function(input, callback){
-    $http.post('/getOrg', input).then(function(output){
-      callback(output.data);
-    });
-  }
+  factory.getShow = function(input, callback){
+     $http.post('/getShow', input).then(function(output){
+       callback(output);
+     });
+   };
 
-
-return factory;
-
-}); // End showPageFactory Factory
+    return factory;
+});
